51bbaf1df58b73790a15eed6212ab9835caf66ad
Jeremy Leif <jeremy.leif@gmail.com>
2017-03-13 13:56:31 -0500

    I think this is base mode , can add and delete

--- server/public/scripts/controllers/hero-list.js
+++ server/public/scripts/controllers/hero-list.js
@@ -1,3 +1,58 @@
-app.controller('HeroListController', function(){
+app.controller('HeroListController',['$http', function($http){
     console.log('Hero List Controller loaded');
+var self = this;
+
+
+
+ self.heroesList = [];
+self.newHero = {};
+getHero();
+function getHero(){
+  $http({
+    method: 'GET',
+    url: '/heroes',
+  }).then(function(response) {
+    console.log(response.data);
+    self.heroesList = response.data;
+  });
+}
+
+
+
+self.addHero =function(){
+    $http({
+    method: 'POST',
+    url:'/heroes',
+    data:self.newHero
+  }).then(function(response){
+    console.log(response);
+    getHero();
+    self.newHero = {};
+  });
+}
+
+
+self.deleteHero = function(heroId){
+  $http({
+  method: 'DELETE',
+  url:'/heroes/'+ heroId,
+
+}).then(function(response){
+  console.log(response);
+  getHero();
+
 });
+}
+
+
+
+
+
+
+
+
+
+
+
+
+}]);
